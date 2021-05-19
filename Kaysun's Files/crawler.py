9c7ba30f477f46d9806b1dc3ae3addf5a79a7a87
QUAKEKING <84090738+Kaysun-QK@users.noreply.github.com>
2021-05-20 01:27:48 +0430

    Update crawler.py

--- Kaysun's Files/crawler.py
+++ Kaysun's Files/crawler.py
@@ -1,4 +1,8 @@
+# This crawler is a test(for now) for getting all the datas from games on https://store.steampowered.com
+
+import re
 from os import link
+from typing import Pattern
 import requests
 from bs4 import BeautifulSoup
 
@@ -8,8 +12,19 @@ def steamSpider():
         sourceCode = requests.get(url)
         plainText = sourceCode.text
         soup = BeautifulSoup(plainText)
-        for link in soup.findAll('div', {'class': 'col search_price responsive_secondrow' }):
-         titles = link.string
-         print(titles)
+        Pattern = r'^https://store.steampowered.com/app/'
+        games = soup.findAll('a', href=re.compile(Pattern))
+         #titles = link
+        print(games)
         
 steamSpider()
+
+
+def main():
+    url = 'https://store.steampowered.com/search/results/?query&start=0&count=100'
+
+
+
+# all the games on steam with page search "https://store.steampowered.com/search/?page=2"
+# a very important url: "https://store.steampowered.com/search/results/?query&start=0&count=100" 
+# add "&infinite=1" in the end of the url above to make it infinite
